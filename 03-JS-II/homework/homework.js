22cfe635c568e6bfe2127761ccb6499231463d7e
FranciscoM7 <97486299+FranciscoM7@users.noreply.github.com>
2022-01-24 20:40:45 -0600

    Update homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -180,20 +180,32 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if (valor == true) {
+    return "Soy verdadero";
+  } else if (valor == false) {
+    return "Soy falso";
+  }
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  var multiplosDeSeis = [];
+  for (let i=0; i <= 10; i++) {
+    multiplosDeSeis[i] = i * 6;
+  }
+   return multiplosDeSeis; 
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+ var len = numero.toString().length;
+ if (len === 3) {
+   return true;
+ }
+  return false;
 }
 
 function doWhile(numero) {
