01b50493d191f76515679ca4fd948713e25d299c
FranciscoM7 <fcoj.mog@gmail.com>
2022-02-10 22:12:47 -0600

    First commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -75,7 +75,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.lenght == str2.lenght) {
+  if (str1.length == str2.length) {
     return true;
   } else {
     return false;
@@ -260,7 +260,7 @@ function esVocal(letra){
   //Escribe tu código aquí
   
   if (letra.length > 1) {
-    return "Dato";
+    return "Dato incorrecto";
   } 
   const vocal = ["a", "e", "i", "o", "u"];
   
